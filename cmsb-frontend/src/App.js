a7aeeceeef7b3af0c4f3d741e76c65c82004a8a3
christorejisam <christorejisam27@gmail.com>
2022-04-06 09:14:51 +0530

    Connecting to MetaMask

--- cmsb-frontend/src/App.js
+++ cmsb-frontend/src/App.js
@@ -1,22 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Button } from 'react-bootstrap'
+import useMetaMask from './hooks/useMetaMask';
 
 function App() {
+
+  const { connect, disconnect, isActive, account } = useMetaMask()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="secondary" onClick={connect}>
+          <img src="images/metamask.svg" alt="metamask" width="50" height="50" /> Connect to MetaMask
+        </Button>
+        <div className="mt-2 mb-2">
+          Connected Account: { isActive ? account : '' }
+        </div>
+        <Button variant="danger" onClick={disconnect}>
+          Disconnect MetaMask<img src="images/noun_waving_3666508.svg" width="50" height="50" />
+        </Button>
       </header>
     </div>
   );

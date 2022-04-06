431b2afe2e8e19e8d9bc4eac61c3e9d06c4322e9
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2022-04-06 17:16:48 +0530

    Fixed metamask connection

--- cmsb-frontend/src/App.js
+++ cmsb-frontend/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
-import './App.css'
-import { Button } from 'react-bootstrap'
-import useMetaMask from './hooks/useMetaMask';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import useMetaMask from './hooks/metamask';
+import { Contract, providers } from 'ethers';
 
 function App() {
 
-  const { connect, disconnect, isActive, account } = useMetaMask()
+  const { connect, disconnect, isActive, account, library } = useMetaMask();
+
+  const nconnect = async () => {
+    console.log(library.provider)
+    const provider = new providers.Web3Provider(library.provider);
+    console.log(provider)
+    connect();
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <Button variant="secondary" onClick={connect}>
-          <img src="images/metamask.svg" alt="metamask" width="50" height="50" /> Connect to MetaMask
+        <Button variant="secondary" onClick={nconnect}>
+          <img src={logo} alt="metamask" width="50" height="50" /> Connect to MetaMask
         </Button>
         <div className="mt-2 mb-2">
           Connected Account: { isActive ? account : '' }
         </div>
         <Button variant="danger" onClick={disconnect}>
-          Disconnect MetaMask<img src="images/noun_waving_3666508.svg" width="50" height="50" />
+          Disconnect MetaMask
         </Button>
       </header>
     </div>
--- cmsb-frontend/src/index.js
+++ cmsb-frontend/src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 import App from './App';
 
 import Web3 from 'web3';
-import { Web3ReactProvider } from '@web3-react/core'
-import { MetaMaskProvider } from './hooks/useMetaMask'
+import { Web3ReactProvider } from '@web3-react/core';
+import { MetaMaskProvider } from './hooks/metamask';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function getLibrary(provider, connector) {
-  return new Web3(provider)
+  return new Web3(provider);
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <Web3ReactProvider getLibrary={getLibrary}>
       <MetaMaskProvider>
-    <App />
+        <App />
       </MetaMaskProvider>
     </Web3ReactProvider>
   </React.StrictMode>,
